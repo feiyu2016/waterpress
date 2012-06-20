662b40b186242d9382e1d82c75e310d12aaf3049
Paper <ipaper@live.cn>
2012-06-20 22:08:29 +0800

    support pure text articles

--- tag.php
+++ tag.php
@@ -16,10 +16,12 @@
     <div id="more"><span> 查 看 更 多 >> </span></div>
     <script id="tpl" type="text/template">
         <div class="item" id="item-${id}" style="top:${top}px;left:${left}px;">
-           <div class="title"><a href="${href}" target="_blank">${title}</a></div>
-           <div class="image"><a href="${href}" target="_blank"><img width="190" height="${height}" src="${image}" alt="${title}" /></a></div>
-           <div class="excerpt">${excerpt}</div>
-           <div class="attr"><a class="category" href="">${category}</a> / <a class="comments" href="">${comments} COMMENTS</a></div>
+            <div class="title"><a href="${href}" target="_blank">${title}</a></div>
+            {@if height > 0}
+            <div class="image"><a href="${href}" target="_blank"><img width="190" height="${height}" src="${image}" alt="${title}" /></a></div>
+            {@/if}
+            <div class="excerpt">${excerpt}</div>
+            <div class="attr"><a class="category" href="">${category}</a> / <a class="comments" href="">${comments} 评论</a></div>
         </div>
     </script>
     <?php get_footer(); ?>
@@ -40,25 +42,36 @@
                 $image_url = get_first_image();
 
                 if ( $image_url ) {
-
                     $image_path = parse_url($image_url);
                     $image_size = getimagesize($doc_root . $image_path['path']);
                     $height = floor(190 / $image_size[0] * $image_size[1]);
-
-                    $category = get_the_category();
-
-                    $item = array(
-                        "href"=>$post->guid,
-                        "image"=>get_bloginfo('template_url') .'/timthumb.php?src='. $image_url .'&w=190&zc=1',
-                        "title"=>$post->post_title,
-                        "height"=>$height,
-                        "excerpt"=>$post->post_excerpt,
-                        "category"=>$category[0]->name,
-                        "comments"=>$post->comment_count
-                    ); 
-                    array_push($json, $item);
+                    $thumb = get_bloginfo('template_url') .'/timthumb.php?src='. $image_url .'&w=190&zc=1';
+                } else {
+                    $thumb = '';
+                    $height = 0;
                 }
+
+                if ( $post->post_excerpt ) {
+                    $excerpt = $post->post_excerpt;
+                } else {
+                    $excerpt = mb_strimwidth(strip_tags(apply_filters('the_content', $post->post_content)), 0, 220, '...');
+                }
+
+                $category = get_the_category();
+
+                $item = array(
+                    "href"=>$post->guid,
+                    "image"=>$thumb,
+                    "title"=>$post->post_title,
+                    "height"=>$height,
+                    "excerpt"=>$excerpt,
+                    "category"=>$category[0]->name,
+                    "comments"=>$post->comment_count
+                ); 
+                array_push($json, $item);
+
             endwhile;
+
             echo json_encode($json);
         }
     ?>
